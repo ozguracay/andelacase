8abdc225822e06891d4af9743cd79579a9eb0278
ozgur serhat acay <ozgurserhatacay@ozgurs-MacBook-Pro.local>
2022-12-03 21:23:35 +0300

    add unzip operation file

---
+++ unzip_data.py
@@ -0,0 +1,4 @@
+from src.utils import unzip_file
+
+unzip_file("storage/compressed_data/application_record.csv.zip", "storage/data")
+unzip_file("storage/compressed_data/credit_record.csv.zip", "storage/data")
